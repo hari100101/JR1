26b828dee1cc5ae201eb5eb1712e07c1f90cbebb
R542456 <R542456@VW10A26932.principalusa.corp.principal.com>
2022-03-09 12:25:50 +0530

    proj has been added by A

---
+++ github_pull/src/github_pull/Demo.java
@@ -0,0 +1,18 @@
+package github_pull;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList mylist=new ArrayList();
+
+		mylist.add(100);
+		mylist.add("hp");
+		mylist.add(3.45);
+		System.out.println(mylist);
+	}
+
+}
